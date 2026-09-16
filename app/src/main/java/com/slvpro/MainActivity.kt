8c5306219c62de66a3659ed3b4e4bce8fb114cd2
rahulbarinala1-hue <rahulbarinala1@gmail.com>
2026-09-16 12:47:02 +0530

    Add MainActivity for SLVPro application

---
+++ app/src/main/java/com/slvpro/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.slvpro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                Surface {
+                    SLVProFullApp()
+                }
+            }
+        }
+    }
+}
